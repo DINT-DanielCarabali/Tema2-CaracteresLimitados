921d00fcead72ae2229f05f734393fb98ba65f0b
DANIEL STIVEN CARABALI ORDÓÑEZ <dancarord@alu.edu.gva.es>
2022-10-01 16:54:01 +0200

    subo ejercicio tema 2 caracteres limitados

---
+++ Tema2-CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textoTextBox.IsReadOnly = false;
+        }
+
+        private void textoTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int contador = textoTextBox.Text.Length,
+                longitudMaxima = textoTextBox.MaxLength;
+            while (contador < longitudMaxima)
+            {
+                contador++;
+                if (contador == longitudMaxima)
+                    textoTextBox.IsReadOnly = true;
+            }
+        }
+    }
+}
